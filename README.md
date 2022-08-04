66f2343171657334b075b5a95355dceafc4b05ee
Yaroslav Havryliv <yaroslav.h@mate.academy>
2022-08-04 15:37:14 +0300

    [Python Core/File Handling]: task with CleanUpFile context manager created

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# Python boilerplate for GitHub tasks
-
 - **Please note:** read [the guideline](https://github.com/mate-academy/py-task-guideline/blob/main/README.md)
 before starting.
 - Implement the task described [here](app/main.py)
+
+Create a custom context manager:
+
+In this task you should implement your own CleanUpFile context manager as a class. 
+It should remove the file if it exists after exiting it.
+
+Its constructor should take only the filename parameter. 
+Also CleanUpFile must have __enter__ (returns itself) and __exit__ (deletes a file if it exists) methods.
+
+Example:
+```
+with open(“file.txt”, “w”) as file:
+    with CleanUpFile(“file.txt”):
+        file.write(“Hello Mate!”)
+```
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,38 @@
-# TODO: add tests
-from app.main import hello_world
+import os.path
+
+import pytest
+
+from app.main import CleanUpFile
+
+filename = "file.txt"
 
 
-def test_hello_world():
-    assert hello_world() == "Hello, world!"
+@pytest.fixture()
+def clean_up():
+    clean_up = CleanUpFile(filename)
+    return clean_up
+
+
+def test_if_file_is_deleted():
+    with open(filename, "w") as f:
+        with CleanUpFile(filename):
+            f.write("111")
+
+    assert os.path.exists(filename) is False, "File should be deleted"
+
+
+def test_check_if_file_is_deleted_with_exception():
+    with open(filename, "w") as f:
+        with CleanUpFile(filename):
+            with pytest.raises(ZeroDivisionError):
+                x = 1 / 0
+
+    assert os.path.exists(filename) is False, "File should be deleted despite exceptions"
+
+
+def test_clean_up_file_constructor(clean_up):
+    assert clean_up.filename == "file.txt", f"self.filename should equal filename given to constructor"
+
+
+def test_clean_up_file_enter_method(clean_up):
+    assert clean_up.__enter__() == clean_up, "__enter__ should return class instance"
